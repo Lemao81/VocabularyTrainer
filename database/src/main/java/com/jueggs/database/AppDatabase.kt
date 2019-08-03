2ac27e8cee371e345a6d290b76123cb4f07d2277
Lemao81 <jr.berlin@web.de>
2019-08-03 20:44:40 +0200

    add database singleton creation

--- database/src/main/java/com/jueggs/database/AppDatabase.kt
+++ database/src/main/java/com/jueggs/database/AppDatabase.kt
@@ -1,6 +1,8 @@
 package com.jueggs.database
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.jueggs.database.entities.FlashCardEntity
 import com.jueggs.database.storagelayers.FlashCardStorageLayer
@@ -8,4 +10,21 @@ import com.jueggs.database.storagelayers.FlashCardStorageLayer
 @Database(entities = [FlashCardEntity::class], version = 1)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun getFlashCardStorageLayer(): FlashCardStorageLayer
+
+    companion object {
+        private var INSTANCE: AppDatabase? = null
+        private const val NAME = "vocabularytrainer.db"
+
+        fun getInstance(context: Context): AppDatabase {
+            if (INSTANCE == null) {
+                synchronized(AppDatabase::class) {
+                    INSTANCE = Room.databaseBuilder(context.applicationContext, AppDatabase::class.java, NAME)
+                        .allowMainThreadQueries()
+                        .build()
+                }
+            }
+
+            return INSTANCE!!
+        }
+    }
 }
