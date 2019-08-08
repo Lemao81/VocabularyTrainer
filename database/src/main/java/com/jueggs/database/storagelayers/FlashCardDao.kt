f62faacc6f152529334386f26ee2a189e9c0cf17
Lemao81 <jr.berlin@web.de>
2019-08-08 23:57:03 +0200

    add flashcard entity update + delete methods

--- database/src/main/java/com/jueggs/database/storagelayers/FlashCardDao.kt
+++ database/src/main/java/com/jueggs/database/storagelayers/FlashCardDao.kt
@@ -1,11 +1,22 @@
 package com.jueggs.database.storagelayers
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Query
+import androidx.room.Update
 import com.jueggs.database.entities.FlashCardEntity
 
 @Dao
 interface FlashCardDao {
+    @Query("SELECT * FROM flashcardentity WHERE id = :id")
+    fun readById(id: Long): FlashCardEntity
+
     @Query("SELECT * FROM flashcardentity WHERE box_number = :boxNumber AND last_learned_date < :expiryDate")
     fun readByBoxNumberAndExpiryDate(boxNumber: Int, expiryDate: Long): List<FlashCardEntity>
+
+    @Update
+    fun update(card: FlashCardEntity)
+
+    @Delete
+    fun delete(card: FlashCardEntity)
 }
