2ed3e46df3add293a3b9d023816e1c9adf6cfdd6
Lemao81 <jr.berlin@web.de>
2019-09-17 19:59:45 +0200

    add usecase test tag

--- domain/src/test/kotlin/com/jueggs/domain/helper/AbstractUseCaseInnerClassTest.kt
+++ domain/src/test/kotlin/com/jueggs/domain/helper/AbstractUseCaseInnerClassTest.kt
@@ -1,7 +1,9 @@
 package com.jueggs.domain.helper
 
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Tag
 
+@Tag("usecase")
 abstract class AbstractUseCaseTestInnerClass<TClassUnderTest : Any, TViewState>(
     private val nestingClass: AbstractUseCaseTest<TClassUnderTest, TViewState>
 ) {
