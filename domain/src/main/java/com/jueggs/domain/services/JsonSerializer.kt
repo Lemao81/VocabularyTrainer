7494fda63a981d65d88378715b9257de0c6f50f2
Lemao81 <jr.berlin@web.de>
2019-10-05 15:15:43 +0200

    add serializer toJson method

--- domain/src/main/java/com/jueggs/domain/services/JsonSerializer.kt
+++ domain/src/main/java/com/jueggs/domain/services/JsonSerializer.kt
@@ -1,16 +1,21 @@
 package com.jueggs.domain.services
 
+import com.jueggs.domain.services.interfaces.ISerializer
 import kotlinx.serialization.ImplicitReflectionSerializer
+import kotlinx.serialization.internal.StringSerializer
 import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonElement
 import kotlinx.serialization.list
+import kotlinx.serialization.map
 import kotlinx.serialization.modules.getContextualOrDefault
 import kotlin.reflect.KClass
 
 @ImplicitReflectionSerializer
-class JsonSerializer(
-    private val json: Json
-) : com.jueggs.domain.services.interfaces.ISerializer {
+class JsonSerializer(private val json: Json) : ISerializer {
     override fun <T : Any> stringify(objects: List<T>, klass: KClass<T>): String = json.stringify(json.context.getContextualOrDefault(klass).list, objects)
 
     override fun <T : Any> parseList(objects: String, klass: KClass<T>): List<T> = json.parse(json.context.getContextualOrDefault(klass).list, objects)
+
+    override fun <T : Any> toJson(map: Map<T, Any>, klass: KClass<T>): JsonElement =
+        json.toJson((json.context.getContextualOrDefault(klass) to StringSerializer).map, map.mapValues { it.value.toString() })
 }
