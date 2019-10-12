18201c43306af2018397d7927e9fef7c806eb02a
Lemao81 <jr.berlin@web.de>
2019-10-12 20:52:15 +0200

    rename service package name

--- app/src/prod/java/com/jueggs/vocabularytrainer/BuildVariantKoinModule.kt
+++ app/src/prod/java/com/jueggs/vocabularytrainer/BuildVariantKoinModule.kt
@@ -2,8 +2,8 @@ package com.jueggs.vocabularytrainer
 
 import com.jueggs.domain.services.ProdFlashCardBoxService
 import com.jueggs.domain.services.interfaces.IFlashCardBoxService
-import com.jueggs.vocabularytrainer.services.ProdAlarmService
-import com.jueggs.vocabularytrainer.services.interfaces.IAlarmService
+import com.jueggs.vocabularytrainer.domainservices.ProdAlarmService
+import com.jueggs.vocabularytrainer.domainservices.interfaces.IAlarmService
 import org.koin.dsl.module
 
 val buildVariantKoinModule = module {
