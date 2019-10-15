9152218f1f402c22df9326dc1a70c986ae97a24b
Lemao81 <jr.berlin@web.de>
2019-10-15 22:16:24 +0200

    add visibility attributes to visibility dependent views

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -7,7 +7,7 @@ object Versions {
     const val hugoPlugin = "1.2.1"
 
     // utils
-    const val andutils = "1.0.134"
+    const val andutils = "1.0.135"
     const val andtestutils = "1.0.11"
     const val jutils = "1.0.34"
     const val jtestutils = "1.0.0"
